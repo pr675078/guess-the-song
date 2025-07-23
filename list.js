a7ab52c587b35b9b645552c5da2ec72a49c8532d
Pragya Rajput <pr675078@gmail.com>
2025-07-23 22:16:59 +0530

    Update list.js

--- list.js
+++ list.js
@@ -1,16 +1,16 @@
 const songlist = [
-    { name: "shape of you", emoji: "🔵+( )+U", audio: "f9f0-7198-463d-8de5-eb9d480623e2.mp3" },
-    { name: "kala chasma", emoji: "🖤🕶 ", audio: "e7ce-9ca9-475a-b563-16d2e3f4c0b2.mp3" },
+    
+    { name: "kala chashma", emoji: "🖤🕶 ", audio: "e7ce-9ca9-475a-b563-16d2e3f4c0b2.mp3" },
     { name: "laal pari", emoji: "❤️🧚🏻‍♀️", audio: "7bbf-5bd9-45ad-a293-40b71f0f04b3.mp3" },
     { name: "neend churai meri", emoji: "😴👮🏻‍♂️🤔", audio: "69f9-02a5-4e9f-8c94-f519a35c55c5.mp3" },
-    { name: "dil to baccha hai ji", emoji: "🫀=👶🏻", audio: "23d1-f8b0-4918-a94a-e6dc08a49614.mp3" },
+    { name: "dil to bachcha hai ji", emoji: "🫀=👶🏻", audio: "23d1-f8b0-4918-a94a-e6dc08a49614.mp3" },
     { name: "selfie le le re", emoji: "🤳🏻", audio: "selfie-le-le-re-bajrangi-bhaijaan-128-kbps_yjgjuboY.mp3" },
     { name: "mera joota hai japani", emoji: "👞= 🎌", audio: "mera-joota-hai-japani-shree-420-128-kbps_JNH7HkDi.mp3" },
     { name: "the breakup song", emoji: "💔🎶", audio: "the-breakup-song-ae-dil-hai-mushkil-128-kbps_FAViElFD.mp3" },
-    { name: "idhar chala mai udhar chala", emoji: "🚶‍♂️🚶‍♂️‍➡️🤷‍♂️🚶‍♂️", audio: "idhar-chala-mein-udhar-chala-koi-mil-gaya-2003-128-kbps_4I7AMNJp.mp3" },
+    { name: "idhar chala main udhar chala", emoji: "🚶‍♂️🚶‍♂️‍➡️🤷‍♂️🚶‍♂️", audio: "idhar-chala-mein-udhar-chala-koi-mil-gaya-2003-128-kbps_4I7AMNJp.mp3" },
     { name: "tan tana tan tan tan tara", emoji: "🔟🔟🔟🔟🔟⭐🚶🏻‍♀️🕘🕛", audio: "chalti-hai-kya-9-se-12-judwaa-2-128-kbps_VLrXgEuM.mp3" },
     { name: "tu cheez badi hai mast mast", emoji: "2️⃣+🧀=👌🏻👌🏻", audio: "cheez-badi-machine-128-kbps_1u3VFH6e.mp3" },
-    { name: "ankhein khuli ho ya ho band", emoji: "👀 ya ☺️", audio: "aankhein-khuli-mohabbatein-128-kbps_3KyYryio.mp3" },
-    { name: "chittiyan kalaaiyaan ve", emoji: "✉+🤜🏻", audio: "chittiyaan-kalaiyaan-roy-128-kbps_r3PldLaK.mp3" },
-    { name: "tip tip barsa pani", emoji: "💧+💧=🔥", audio: "tip-tip-sooryavanshi-128-kbps_XIVEnCLL.mp3" },
+    { name: "aankhein khuli ho ya ho band", emoji: "👀 ya ☺️", audio: "aankhein-khuli-mohabbatein-128-kbps_3KyYryio.mp3" },
+    { name: "chittiyaan kalaiyaan", emoji: "✉+🤜🏻", audio: "chittiyaan-kalaiyaan-roy-128-kbps_r3PldLaK.mp3" },
+    { name: "tip tip barsa paani", emoji: "💧+💧=🔥", audio: "tip-tip-sooryavanshi-128-kbps_XIVEnCLL.mp3" },
 ];
